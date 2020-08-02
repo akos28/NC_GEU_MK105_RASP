06c5b717cdb814fb54b947b5ddcff73302a83794
Saudamini <31340723+saudamini101@users.noreply.github.com>
2020-08-02 15:24:03 +0530

    Create test.py

---
+++ src/Philomath/test.py
@@ -0,0 +1,11 @@
+"""
+	Testing program for Continuous Integration provided by Travis-CI for:
+	TeamRasp/Philomath
+"""
+
+print ("Begin testing...")
+
+# Test scripts go here:
+
+
+print ("Testing complete!")
